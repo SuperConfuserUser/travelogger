f304c7bfeac4fbc254916a77220eaa9ea67d00ac
unenlightened <adept_inept@yahoo.com>
2018-05-10 13:49:37 -0400

    new user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :trips
+  validates_associated :trips
 
   has_secure_password
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, allow_blank: true, uniqueness: { case_sensitive: false }
 end
