04dbab5e0de8b3b7fc5c19e9ebf14fdea9f37d86
unenlightened <adept_inept@yahoo.com>
2018-05-11 16:35:06 -0400

    omniauth and traditional registration works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.username = auth['info']['name']
-      u.email = auth['info']['email']
+    
+    @user = User.find_or_create_by_omniauth(auth)
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to new_user_path, alert: "Facebook didn't work."
     end
-
-byebug
-
-    session[:user_id] = @user.id
-
-    redirect_to @user
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-  validates :email, allow_blank: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+
+  def self.find_or_create_by_omniauth(auth)
+    u = find_by(uid: auth['uid'])
+
+    if !u
+      u = User.new
+      u.uid = auth['uid']
+      u.username = auth['info']['name']
+      u.email = auth['info']['email']
+      #can also bypass validations with 
+      u.save(validate: false)
+    end
+
+    u
+  end
 end
