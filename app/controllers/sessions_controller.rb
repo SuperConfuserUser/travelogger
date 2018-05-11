9e7692f1954b222bc52c2a38c48f47baadeb8f2e
unenlightened <adept_inept@yahoo.com>
2018-05-11 18:16:40 -0400

    session routes and starting on tradition login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,34 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    
-    @user = User.find_or_create_by_omniauth(auth)
+    # @user = User.find_or_create_by_omniauth(auth)
 
-    if @user
+    # if @user
+    #   session[:user_id] = @user.id
+    #   redirect_to @user
+    # else
+    #   redirect_to new_user_path, alert: "Facebook didn't work."
+    # end
+    byebug
+
+    @user = User.find_by(params[:user][:username]) || User.new
+
+    
+    if @user && @user.authenticate(params[:user][:password]) 
       session[:user_id] = @user.id
       redirect_to @user
     else
-      redirect_to new_user_path, alert: "Facebook didn't work."
+      redirect_to login_path, alert: "no workies"
     end
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to root_path
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
 
   get '/auth/facebook/callback', to: 'sessions#create', constraints: { protocol: /https/ }
 
-
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'register', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   resources :entries
   resources :trips
   resources :users, only: ['index', 'show', 'new', 'create', 'edit', 'update']
 
-  get 'static/index'
   root 'static#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
