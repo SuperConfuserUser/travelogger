284f48bb968e109591b7d0036cb4c2be36b513fc
unenlightened <adept_inept@yahoo.com>
2018-06-15 02:13:09 -0400

    updated new trips and edit trips is mostly working

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   has_many :trip_categories
   has_many :trips, through: :trip_categories
+
+  def formatted_name
+    name.capitalize
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,19 +7,25 @@ class Trip < ApplicationRecord
 
   accepts_nested_attributes_for :locations, :allow_destroy => true 
 
-  before_save :reject_blank_locations  #use this instead of "reject_if: proc {|attributes| attributes['name'].blank?}" or "reject_if: :all_blank" to be able to create multiple blank fields for form input. otherwise, user has to fill field, then add one by one
-
   validates :name, presence: true
-  validate :has_location
+  validate :has_location?
   validates :start_date, presence: true
   validate :end_date_not_before_start_date
-  validate :has_category
+  validate :has_category?
 
+  # Use this instead of "reject_if: proc {|attributes| attributes['name'].blank?}" or "reject_if: :all_blank" to be able to create multiple blank fields for form input. Otherwise, user has to fill field, then add one by one
+  before_save :reject_blank_locations!
   
-  def has_location
+  def has_location?
     named_locations = locations.count { |location| location.name.present? }
     if named_locations == 0
       errors.add(:locations, "can't be blank")
+    end
+  end
+
+  def reject_blank_locations!
+    locations.each do |location|
+      location.destroy if location.name.blank?
     end
   end
 
@@ -29,22 +35,9 @@ class Trip < ApplicationRecord
     end
   end
 
-  def category_ids=(ids)
-    ids.each do |id|
-      category = Category.find(id)
-      self.categories << category
-    end
-  end
-
-  def has_category
-    if category_ids.none?
+  def has_category?
+    if category_ids.none? # if it's only the hidden field
       errors.add(:categories, "need to be chosen")
-    end
-  end
-
-  def reject_blank_locations
-    locations.each do |location|
-      location.destroy if location.name.blank?
     end
   end
 
