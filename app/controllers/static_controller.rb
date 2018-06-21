bee9d875df56a7ebc3708934e2816854a275b0f8
unenlightened <adept_inept@yahoo.com>
2018-06-21 03:41:11 -0400

    added footer and navigation partials and styling

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,5 @@
-class StaticController < ApplicationController
+class StaticController < ApplicationController  
   def index
     redirect_to trips_path if logged_in?
-    render :layout => "cover_page"
   end
 end
