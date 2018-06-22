55a9b6ec2b26c07c4dc841bbc428fdad0d6e07ff
unenlightened <adept_inept@yahoo.com>
2018-06-21 22:18:46 -0400

    refactored login/out and error msg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,22 @@
 module ApplicationHelper
+  # LAYOUT
   def shared_navigation
     render 'shared/navigation' if !current_page?('/')
+  end
+
+  def conditional_navigation(logged_in)
+    if logged_in
+      render 'shared/navigation_logged_in'
+    else 
+      render 'shared/navigation_logged_out'
+    end
+  end
+  
+  def sign_up_link
+    link_to "Sign up", signup_path
+  end
+
+  def log_in_link
+    link_to "Log in", login_path
   end
 end
