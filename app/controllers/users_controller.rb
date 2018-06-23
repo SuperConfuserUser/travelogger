ddd139ddf8a958f34f04248e9dbd4e8b69e14a1e
unenlightened <adept_inept@yahoo.com>
2018-06-23 19:10:54 -0400

    refactored user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @latest_limit = 5;
   end
 
   def new
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,5 @@
 module UsersHelper
+  #FORM
   def user_form(user)
     render 'user_form', user: user
   end
@@ -15,6 +16,7 @@ module UsersHelper
     user.new_record? ? "Password Confirm" : "New Password Confirm"
   end
 
+  #INDEX and SHOW
   def user_trips_count_link(user)
     link_to "(#{pluralize(user.trips.count, 'Trip')})", user_trips_path(user)
   end
@@ -27,4 +29,40 @@ module UsersHelper
     user.created_at
   end
 
+  #SHOW
+  def user_tagline(user)
+    if user.tagline?
+      user.tagline
+    elsif authorized?(user)
+      link_to "Add tagline", edit_user_path(user)
+    else
+      "A traveller. A wanderer."
+    end
+  end
+
+  def user_profile(user)
+    if user.profile?
+      user.profile
+    else 
+      link_to "Add profile", edit_user_path(user) if authorized?(user)
+    end 
+  end
+
+  def user_more_trips_link(user, latest_limit)
+    link_to "More trips", user_trips_path(user) if user.trips.count > latest_limit
+  end
+
+  def user_latest_trips(user, latest_limit)
+    if user.trips.any?
+      render "users/user_show_trips_listing", user: user, latest_limit: latest_limit 
+    elsif user.profile.nil?
+      content_tag :p, "Nothing to see here."
+    end
+  end
+
+  def user_edit_link(user, klass)
+    link_to "Edit account", edit_user_path(current_user), class: klass if authorized?(user)
+  end
+
 end
+
