dac7d6868d5a8140fc57892a4c634d8dfbfda2e6
unenlightened <adept_inept@yahoo.com>
2018-06-24 02:33:55 -0400

    styled alert and cleaned strays

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :user_id
-    redirect_to root_path
+    redirect_to root_path, alert: "Logged out."
   end
 
   private 
