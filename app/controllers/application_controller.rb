4a799a4032621c714f6ad27961b53f1d6af61570
unenlightened <adept_inept@yahoo.com>
2018-06-25 19:42:39 -0400

    fixed ghost button css and user filter for nested trips index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?, :authorized?, :nested?
 
   def current_user
-    current_user ||= User.find(session[:user_id]) if session[:user_id]
+    current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
