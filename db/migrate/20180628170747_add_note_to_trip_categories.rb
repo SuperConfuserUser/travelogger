9a3655cb629d3cd322ec67c1e97b6a4ac1fd53e7
unenlightened <adept_inept@yahoo.com>
2018-06-28 13:08:51 -0400

    adding note column to trip_categories

---
+++ db/migrate/20180628170747_add_note_to_trip_categories.rb
@@ -0,0 +1,9 @@
+class AddNoteToTripCategories < ActiveRecord::Migration[5.2]
+  def up
+    add_column :trip_categories, :note, :text
+  end
+
+  def down
+    remove_column :trip_categories, :note, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_23_213442) do
+ActiveRecord::Schema.define(version: 2018_06_28_170747) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_06_23_213442) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "note"
     t.index ["category_id"], name: "index_trip_categories_on_category_id"
     t.index ["trip_id"], name: "index_trip_categories_on_trip_id"
   end
