dc33b8a787acad078d0d6d555b6557b0b5642ac8
unenlightened <adept_inept@yahoo.com>
2018-07-31 04:59:01 -0400

    Updating note for location assocations

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,7 @@ class Location < ApplicationRecord
   belongs_to :place, polymorphic: true
 end
 
-# TODO: probably doesn't need to be polymorphic. i wanted trip and entries to own a location, but location can belong to trip. entries has location through location. entries can build location off of trip
+# TODO: probably doesn't need to be polymorphic. I wanted trip and entries to has_many locations, but location can belongs_to trip. entries belongs_to trip and has_many locations through trip. entries can build location off of trip. what relationship should this be then?
+
 
 
