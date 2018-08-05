0d0eed4cf0eeaaa36cfb24f82cf208b6ba23595c
unenlightened <adept_inept@yahoo.com>
2018-08-04 20:29:49 -0400

    removing unused model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
   has_many :trip_categories
   has_many :trips, through: :trip_categories
-  has_many :category_descriptions
 
   def formatted_name
     name.capitalize
