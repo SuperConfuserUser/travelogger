fc3ec628a37ca4df673263b43c0016a008ad7abc
unenlightened <adept_inept@yahoo.com>
2018-08-05 09:21:56 -0400

    formatting trip location list

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -12,15 +12,14 @@ class Trip {
     const currentYear = (new Date).getFullYear();
     const date = new Date(sysDate);
     
-    if(date.getFullYear() === currentYear) {
-      return date.toLocaleDateString('en-us', { month: 'long', day: 'numeric' }); //Month 1
+    if(date.getFullYear() === currentYear) { 
+      return date.toLocaleDateString('en-us', { month: 'long', day: 'numeric' }); // Month 1
     } else {
       return date.toLocaleDateString('en-us', { month: 'long', day: 'numeric', year: 'numeric' }); //Month 1, 2000
     }
   }
 
   tripDateRange() {
-    debugger
     if (this.end_date == null) {
       return this.longDate(this.start_date);
     } else {
@@ -29,18 +28,28 @@ class Trip {
   }
 
   userImage() {
-    debugger
     if(this.user.image.startsWith('http')) {
       return this.user.image;
     } else {
-      return "/assets/" + this.user.image;
+      return "/assets/" + this.user.image;   //adding full path to the default image
+    }
+  }
+
+  locationList() {
+    const locations = this.locations.map( location => location.name );
+    
+    if(locations.length <= 2) {
+      return locations.join(" and ");
+    } else {
+      locations[locations.length - 1] = "and " + locations[locations.length - 1];
+      return locations.join(", ");
     }
   }
 
   renderIndexLi() {
     const source   = $('#trip-index-li-template').html();
     const template = Handlebars.compile(source);
-    const context = { id: this.id, userId: this.user_id, userImage: this.userImage(), name: this.name, userUserName: this.user.username, locations: this.locations, date: this.tripDateRange() };
+    const context = { id: this.id, userId: this.user_id, userImage: this.userImage(), name: this.name, userUserName: this.user.username, locations: this.locationList(), date: this.tripDateRange() };
     const html    = template(context);
     return html;
   }
@@ -98,24 +107,3 @@ const loadTripsIndex = () => {
     $wrapper.append(html);
   })
 }
-
-{/* <li>
-  <%= render 'users/user_avatar_link', user: trip.user, klass: "list-avatar" %>
-
-  <div class="list-description">
-    <%= link_to trip.name, user_trip_path(trip.user, trip), class: "list-title" %>
-    <%= trip_label(trip) %>
-     on <%= date_long(trip.start_date) %>
-  </div><!-- list-description -->
-</li> */}
-
-// nested options for links and trip label
-// def trip_label(trip)
-//     if nested?
-//       trip_location_list(trip)
-//     else
-//       render layout: 'users/user_name_link', locals: { user: trip.user, klass: "camo-link strong" } do
-//         " " + trip_location_list(trip)
-//       end
-//     end 
-//   end 
