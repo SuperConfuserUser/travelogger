0ac3fc758d058a06078db15d941b8d89bd3680f1
unenlightened <adept_inept@yahoo.com>
2018-08-05 01:36:37 -0400

    testing document on load and blocking in trip js class

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,10 +1,42 @@
 // javascript is super weird
 
+// the class/prototype
+
+class Trip {
+  constructor() {
+    this.foo = "bar";
+  }
+
+  foobar() {
+    alert(this.foo);
+  }
+}
+
+function testClass() {
+  let a = new Trip;
+  return a.foobar();
+}
+
+$(testClass);
+
+// DOM
+
 const attachListeners = () => {
   $('#hide_this').on('click', () => hideWhenClicked('#hide_this'));
-  $('#test').on('click', () => alert("whee!"));
+  $('#test').on('click', testClass);
 }
 
 $(attachListeners);
 
 const hideWhenClicked = el => $(el).hide();
+
+// index
+// do this on doc ready? should JS be separated for each Trip page (index, show, create)
+const loadTripsIndex = () => {
+  const wrapper = $('#trips-list');
+  wrapper.empty();
+
+  let listing = listing || $.getJSON("/trips", (trips) => {
+    // set data and templates now
+  })
+}
