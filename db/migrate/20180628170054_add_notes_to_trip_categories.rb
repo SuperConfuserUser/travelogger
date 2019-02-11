9d6ad63f07d719905f60edbf052884d5dcdf42b4
unenlightened <adept_inept@yahoo.com>
2019-02-10 23:33:25 -0800

    update version

--- db/migrate/20180628170054_add_notes_to_trip_categories.rb
+++
@@ -1,9 +0,0 @@
-class AddNoteToTripCategories < ActiveRecord::Migration[5.2]
-  def up
-    add_column :trip_categories, :note, :text
-  end
-
-  def down 
-    remove_column :trip_categories, :note, :text
-  end
-end
